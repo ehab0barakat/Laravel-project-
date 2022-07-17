6ab48e1b0a1fc17e388fdbf85525d27d24b7b482
ehab0barakat <mrehab9797@gmail.com>
2022-07-17 04:47:44 +0200

    final 3

--- resources/views/shopping.blade.php
+++ resources/views/shopping.blade.php
@@ -94,12 +94,23 @@ new post
                             @endif
                         @endif
 
-                        <form action="{{route('m-book.show' , $book->id )}}" method="get">
+                        @if (auth()->user())
+                        @auth
+                          <form action="{{route('m-book.show' , $book->id )}}" method="get">
                             @csrf
                             <button  type="submit">
-                                <img src="{{ $book->image  }}" class="card-img-top" alt="Pictuere Error">
+                              <img src="{{ $book->image  }}" class="card-img-top" alt="Pictuere Error">
                             </button>
                           </form>
+                        @endauth
+                        @else
+                        <form action="{{route('login')}}" method="get">
+                          @csrf
+                          <button  type="submit">
+                            <img src="{{ $book->image  }}" class="card-img-top" alt="Pictuere Error">
+                          </button>
+                        </form>
+                        @endif
 
                         <div class="card-body">
                             <h5 class="card-title">{{ $book->title  }}</h5>
@@ -113,6 +124,7 @@ new post
                             <!-- buy -->
                             <div class="d-grid gap-2 col-6 mx-auto">
 
+                        @if (auth()->user())
 
                             @auth
                             <form action="{{route('cart.store')}}" method="post">
@@ -120,20 +132,24 @@ new post
                                 <input type="hidden" name="book_id" value="{{ $book->id}}">
                                 <input type="submit" value="buy" class="btn btn-warning ">
                             </form>
+
                             @endauth
 
-
+                        @else
                             <form action="{{route('login')}}" method="get">
                                 @csrf
 
                                 <input type="submit" value="buy" class="btn btn-warning ">
                             </form>
 
-
+                        @endif
                             </div>
                             <br>
                             <!-- Fav -->
                             <div class="d-grid gap-2 col-6 mx-auto">
+
+                        @if (auth()->user())
+
                                 @auth
 
                             {!! Form::open(['route' => ['favourites.store' , $book->id ]  , "method" => "POST"]) !!}
@@ -146,18 +162,23 @@ new post
                                 </button>
                               {{-- </form> --}}
                             {!! Form::close() !!}
-                            @endauth
-                            {!! Form::open(['route' => ['login']  , "method" => "get"]) !!}
 
+                            @endauth
+
+                        @else
+
+                            {!! Form::open(['route' => ['login']  , "method" => "get"]) !!}
                               {{-- <form action="{{route('favourites.create')}}" method="post"> --}}
                                 {{-- @csrf --}}
                                 {{-- <input type="hidden" name="book_id" value="{{ $book->id}}"> --}}
                                 <button type="submit" class="btn btn-outline-danger ">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path></svg>
                                 </button>
+
+
                               {{-- </form> --}}
                             {!! Form::close() !!}
-
+                        @endif
                             </div>
                         </div>
                     </div>
